56331d2c77ca9ca6530287e8b9ccf7770704f343
Teo Marcelo <tmarcelojr@gmail.com>
2022-01-25 13:17:56 -0600

    Created Login component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useContext } from 'react'
+import { Routes, Route } from 'react-router-dom'
 // components
 import Nav from './components/Nav'
+// pages
+import Login from './pages/Login'
 // contexts
 import UserContext from './contexts/UserContext.js'
 // css
@@ -17,6 +20,13 @@ function App() {
       {/* All context comes with the Provider Component. This allows us to use this as a wrapper and share information to all of its children. We need the value prop inside our provider. */}
       <UserContext.Provider value={'Christina'}>
         <Nav />
+
+        {/* We need to wrap our all of our routes inside react router Routes component */}
+        <Routes>
+          <Route path='login' element={<Login />} />
+        </Routes>
+
+
       </UserContext.Provider>
 
     </div>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -10,17 +10,17 @@ const Nav = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="#">Pokepedia</Link>
+                <Link className="navbar-brand" to="/">Pokepedia</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to="#">Pokemon List</Link>
+                            <Link className="nav-link active" aria-current="page" to="pokemon/list">Pokemon List</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="#">Login</Link>
+                            <Link className="nav-link" to="login">Login</Link>
                         </li>
                     </ul>
                 </div>
