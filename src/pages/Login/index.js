d03a104da1c5138cd9bc3846992617bc8b113556
Teo Marcelo <tmarcelojr@gmail.com>
2022-01-25 13:47:43 -0600

    created login form

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,8 +1,19 @@
+import './styles.css'
+
 const Login = () => {
     return (
-        <div>
-            LOGIN
-        </div>
+        <form className='mx-auto border p-2 m-2' id='login-form'>
+            <div className="mb-3">
+                <label htmlFor="exampleInputUser1" className="form-label">Username</label>
+                <input type="text" className="form-control" id="exampleInputUser1" aria-describedby="userHelp" />
+                <div id="userHelp" className="form-text">We'll never share your username with anyone else.</div>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                <input type="password" className="form-control" id="exampleInputPassword1" />
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
     );
 }
 
