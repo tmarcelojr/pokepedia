e7c92b3757a7d16fd6b3c181edf662cc27b354ad
Teo Marcelo <tmarcelojr@gmail.com>
2022-01-26 13:01:25 -0600

    added currPagePokemon

--- src/pages/PokemonList/index.js
+++ src/pages/PokemonList/index.js
@@ -1,10 +1,11 @@
 import { useEffect, useState } from 'react';
 import ReactPaginate from 'react-paginate'
+import axios from 'axios'
 
 const PokemonList = ({ pokeList, itemsPerPage }) => {
-    console.log('props', pokeList)
+    // console.log('props', pokeList)
     // We start with an empty list of pokeList.
-    const [currentItems, setCurrentItems] = useState(null);
+    const [currentPokemon, setCurrentPokemon] = useState(null);
     const [pageCount, setPageCount] = useState(0);
     // Here we use item offsets; we could also use page offsets
     // following the API or data you're working with.
@@ -14,9 +15,23 @@ const PokemonList = ({ pokeList, itemsPerPage }) => {
         // Fetch pokeList from another resources.
         const endOffset = itemOffset + itemsPerPage;
         console.log(`Loading pokeList from ${itemOffset} to ${endOffset}`);
-        setCurrentItems(pokeList.slice(itemOffset, endOffset));
+        setCurrentPokemon(pokeList.slice(itemOffset, endOffset));
+
+        // We need to make a conditional so our currPagePokemon doesn't get invoked unless we have data to work with
+        if(currentPokemon) currPagePokemon()
+
         setPageCount(Math.ceil(pokeList.length / itemsPerPage));
     }, [itemOffset, itemsPerPage]);
+
+    const currPagePokemon = () => {
+        currentPokemon.forEach( async (pokemon) => {
+            try {
+
+            } catch (error) {
+
+            }
+        })
+    }
 
     const Pokemon = () => {
         return (
@@ -25,7 +40,7 @@ const PokemonList = ({ pokeList, itemsPerPage }) => {
                     pokeList &&
                     pokeList.map(pokemon => (
                         <div>
-                            <h3>{pokemon.name}</h3>
+                            {/* <h3>{pokemon.name}</h3> */}
                         </div>
                     ))
                 }
@@ -42,13 +57,15 @@ const PokemonList = ({ pokeList, itemsPerPage }) => {
         setItemOffset(newOffset);
     };
 
+    console.log('current pokemon', currentPokemon)
+
     return (
         <div>
             <Pokemon />
             <ReactPaginate
                 nextLabel="next >"
                 onPageChange={handlePageClick}
-                pageRangeDisplayed={3}
+                pageRangeDisplayed={2}
                 marginPagesDisplayed={2}
                 pageCount={pageCount}
                 previousLabel="< previous"
